ee43b1bf4f747ae28e4601384658666a3b9b2e9a
merlin.ho <merlin.ho@kakakocorp.com>
2018-10-05 19:02:53 +0900

    리액트 연습

--- chapter9/redux-practice/src/containers/index.js
+++ chapter9/redux-practice/src/containers/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types'
 import SortMenu from '../components/SortMenu';
 import AddColorForm from '../components/AddColorForm'
@@ -7,43 +8,57 @@ import { addColor, sortColors, rateColor, removeColor } from '../actions'
 import { sortFunction } from '../lib/array-helpers'
 
 // import { Menu, NewColor, Colors } from '../containers'
-export const NewColor = (props, {store}) => 
-    <AddColorForm onNewColor={(title, color) => 
-            store.dispatch(addColor(title, color))
-        }/>
+export const NewColor = (props, { store }) =>
+  <AddColorForm onNewColor={(title, color) =>
+    store.dispatch(addColor(title, color))
+  } />
 
 NewColor.contextTypes = {
-    store: PropTypes.object
+  store: PropTypes.object
 }
 
-export const Menu = (props, {store}) => 
-    <SortMenu sort={store.getState().sort}
-        onSelect={sortBy => 
-            store.dispatch(sortColors(sortBy))
-        }
-    />
+export const Menu = (props, { store }) =>
+  <SortMenu sort={store.getState().sort}
+    onSelect={sortBy =>
+      store.dispatch(sortColors(sortBy))
+    }
+  />
 
 Menu.contextTypes = {
-    store: PropTypes.object
+  store: PropTypes.object
 }
 // const ColorList = ({ colors=[], onRate=f=>f, onRemove=f=>f }) => {
 
-export const Colors = (props, {store}) => {
-    const {colors, sort} = store.getState();
-    const sortedColors = [...colors].sort(sortFunction(sort));
-    return (
-        <ColorList colors={sortedColors}
-            onRate={(id, rating) => 
-                store.dispatch(rateColor(id, rating))
-            }
-            onRemove={(id) => 
-                store.dispatch(removeColor(id))
-            }
-        />
-    )
-}
-    
+export const Colors = connect(
+  state => ({
+    colors: [...state.colors].sort(sortFunction(state.sort))
+  }),
+  dispatch => ({
+    onRate(id, rating) {
+      dispatch(rateColor(id, rating))
+    },
+    onRemove(id) {
+      dispatch(removeColor(id))
+    }
+  })
+)(ColorList)
 
-Colors.contextTypes = {
-    store: PropTypes.object
-}
+// export const Colors = (props, {store}) => {
+//     const {colors, sort} = store.getState();
+//     const sortedColors = [...colors].sort(sortFunction(sort));
+//     return (
+//         <ColorList colors={sortedColors}
+//             onRate={(id, rating) => 
+//                 store.dispatch(rateColor(id, rating))
+//             }
+//             onRemove={(id) => 
+//                 store.dispatch(removeColor(id))
+//             }
+//         />
+//     )
+// }
+
+
+// Colors.contextTypes = {
+//     store: PropTypes.object
+// }
