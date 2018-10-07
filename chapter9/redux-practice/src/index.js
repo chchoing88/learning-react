3020039a6d7df1d504b69793ad2741bb893978e0
merlin.ho <merlin.ho@kakaocorp.com>
2018-10-07 15:50:49 +0900

    리액트 테스트 연습중

--- chapter9/redux-practice/src/index.js
+++ chapter9/redux-practice/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render } from 'react-dom'
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './components/App';
 import storeFactory from './store';
 
